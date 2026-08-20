e1053603f481e56d683d0ed1ace48d7b7f6eff7c
sanlith340-droid <sanlith340@gmail.com>
2026-08-20 14:20:14 -0500

    0.2

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PUERTO || 7777; 
-
+const port = process.env.PUERTO || 7777;
+const sistemaArchivo = require('fs');
+const ruta = require('path');
+const rutaArchivoJson = ruta.join(__dirname, 'lista_datos.json');
 
 app.get('/', (req, res) => {
     res.send('API RESTFUL = CRUD aprendices');
 });
 
+app.get("/api/aprendices", (req, res) => {
+    const ListaAprendices = []
+    sistemaArchivo.readFile(rutaArchivoJson, "utf-8", (error, datos) => {
+        if (error) {
+        return res.status(500).json({ error: "Error al leer el archivo, conexion bd" });
+    }
+    res.json(JSON.parse(datos));
+    });
+});
 
 app.listen(port, () => {
     console.log(`Servidor en http://localhost:${port}`);
