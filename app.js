7f8b6c40b0a85c1b81f72479ce0cae64fd8d1692
Tu Nombre <tu_correo@example.com>
2026-08-27 14:12:05 -0500

    nuevas peticiones

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 require('dotenv/config')
 const port = process.env.PUERTO || 4000;
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.get('/', (req, res) => {
-    res.send('API RESTFUL = CRUD aprendices');
+app.post('/datosJson', (req, res) => {
+    const datosRecibidos = req.body
+    if (datosRecibidos) {
+        res.status(200).json({mensaje: "datos recibidos correctamente"})
+    }
+    res.status(500).json({Mensaje: "no se recibieron datos"})
 });
+
+app.post('/formulario', (req, res) => {
+    //const datos = req.body
+    const usuario = req.body.usuarios
+    res.json({datos:usuario})
+})
 
 app.listen(port, () => {console.log(`servidor en http://localhost:${port}`); });
